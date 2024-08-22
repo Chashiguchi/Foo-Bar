85487837fb4b598c309f9362d5def71a9977a11b
Chase <Chashiguchi@gmail.com>
2024-08-22 10:03:35 -0500

    added counter

--- Foo Bar/ContentView.swift
+++ Foo Bar/ContentView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var counter = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(counter)")
+                .font(.system(size: 128))
+                .multilineTextAlignment(.center)
+                .preferredColorScheme(.dark)
+                .onTapGesture {
+                    counter += 1
+                }
+                .onLongPressGesture {
+                    counter = 0
+                }
         }
         .padding()
     }
