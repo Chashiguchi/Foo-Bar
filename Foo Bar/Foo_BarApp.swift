77cf8547e51a4d3fba85c86e9abe5416c39b9704
Chase <Chashiguchi@gmail.com>
2024-08-22 09:56:29 -0500

    Initial Commit

---
+++ Foo Bar/Foo_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Foo_BarApp.swift
+//  Foo Bar
+//
+//  Created by Chase Hashiguchi on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Foo_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
